ace2686f5725368d626329dc33167cf2453f636c
Hoang Bui <bmhoang@outlook.com>
2019-06-27 08:18:55 +0700

    basic rsa encrypt decrypt

---
+++ RSATools/RSATools/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using hob.crypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RSATools
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Decrypt(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var rsa = new StandardRSA(privateKey.Text, publicKey.Text);
+                plainText.Text = rsa.Decrypt(cipherText.Text);
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void Encrypt(object sender, RoutedEventArgs e)
+        {
+            try { 
+                var rsa = new StandardRSA(privateKey.Text, publicKey.Text);
+                cipherText.Text = rsa.Encrypt(plainText.Text);
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
